5f58adc670056f31e9489197305536288714497c
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-11-21 22:24:20 +0800

    feat: add support of query name by id

--- src/pages/demo/demo.vue
+++ src/pages/demo/demo.vue
@@ -13,6 +13,13 @@
                 </var-space>
             </template>
         </var-card>
+        <var-card class="mt-4" title="查询学号对应的姓名">
+            <template #default>
+                <var-input v-model="userId" placeholder="请输入学号"></var-input>
+                <var-button class="mt-2" type="info" block @click="summit()">查询</var-button>
+                <p class="mt-2 text-center">{{ userId }} 对应的姓名是 {{ userName }}</p>
+            </template>
+        </var-card>
     </div>
 </template>
 
@@ -24,12 +31,22 @@ export default {
         return {};
     },
     data() {
-        return {};
+        return {
+            userId: null,
+            userName: null
+        };
     },
     created() {},
     mounted() {},
     updated() {},
-    methods: {}
+    methods: {
+        async summit() {
+            const userInfo = await this.$api.user.getUserInfo({
+                userId: this.userId
+            });
+            this.userName = userInfo[0].userName;
+        }
+    }
 };
 </script>
 
